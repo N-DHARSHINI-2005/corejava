972dbb863fc3e013e66aa9ef22c4582fc1eb9bde
Dharshini N <dharshini20050916@gmail.com>
2026-08-12 12:26:48 +0530

    Add files via upload

---
+++ Reversepattern2.java
@@ -0,0 +1,12 @@
+public class Reversepattern2 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int i=1;i<=n-row+1;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
